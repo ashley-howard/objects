7ad9f392c7e2585ce0797ac4d2137e31db486a45
noteconvert <me@ashley.how>
2020-02-09 20:05:06 +0000

    add calendar

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+var monthText = document.getElementById('month');
+var calendarDays = document.getElementById('calendar-days');
+
+const date = new Date;
+const month = date.toLocaleString('default', { month: 'long' });
+const day = date.getDate();
+
+monthText.innerHTML = month;
+
+function daysInThisMonth() {
+    return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+}
+
+for (var i = 0; i < (daysInThisMonth()); i++) {
+    var element = document.createElement("div");
+    element.setAttribute('class', 'grid-item');
+    element.innerHTML = i + 1;
+    if (day - 1 > i) {
+        element.classList.add("checked-day");
+    }
+    calendarDays.appendChild(element);
+}
+
+(function () {
+    function checkTime(i) {
+        return (i < 10) ? "0" + i : i;
+    }
+
+    function startTime() {
+        var today = new Date(),
+            h = checkTime(today.getHours()),
+            m = checkTime(today.getMinutes())
+        document.getElementById('time').innerHTML = h + ":" + m;
+        t = setTimeout(function () {
+            startTime()
+        }, 1000);
+    }
+    startTime();
+})();
